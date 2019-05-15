471d30d985babdf3510d974a0675a5e0b4a84cde
sandeeprawat <sandeeprawat@xebia.com>
2019-05-15 23:20:18 +0300

    files changes code optimized

--- src/main/resources/bitbucket/CommitTrigger.py
+++ src/main/resources/bitbucket/CommitTrigger.py
@@ -13,39 +13,26 @@ import sys
 import json
 import requests
 
-
-def findNewCommit(oldCommitMap, newCommitMap):
-    branch = ""
-    commitId = ""
-
-    # loop over new map branches
-    for newBranch, newCommitId in newCommitMap.iteritems():
-        # check if branch exists in old map
-        if newBranch in oldCommitMap:
-            oldCommitId = oldCommitMap[newBranch]
-            # compare commit ids
-            if newCommitId != oldCommitId:
-                branch = newBranch
-                commitId = newCommitId
-                break
-        else:
-            # new branch, this triggered it
-            branch = newBranch
-            commitId = newCommitId
-            break
-
-    return branch, commitId
-
-def getFileChanges(commit_id):
+def getFileChangesForCommitId(commit_id):
     url_path= "%srest/api/1.0/projects/%s/commits/%s/changes" % (server['url'],repo_full_name,commit_id)
     response = requests.get(url_path, auth=(server['username'], server['password']), headers={}, verify=False)
     if response.status_code == 200:
         changes = json.loads(response.content)
-        return ','.join([v['path']['name'] for v in changes['values']])
+        return [v['path']['name'] for v in changes['values']]
 
+def getFileChanges(old_commit_id, commits):
+    if bool(old_commit_id):
+        result=[]
+        for commit in commits["values"]:
+            commit_id = commit["id"]
+            if commit_id == old_commit_id:
+                break;
+            result = result + getFileChangesForCommitId(commit_id)
+        return ','.join(result)
+    else:
+        return ','.join(getFileChangesForCommitId(commits["values"][0]["id"]))
 
-url_path= "%srest/api/1.0/projects/%s/commits?limit=1&until=refs/heads/%s" % (server['url'],repo_full_name,branchName)
-
+url_path= "%srest/api/1.0/projects/%s/commits?limit=100&until=refs/heads/%s" % (server['url'],repo_full_name,branchName)
 response = requests.get(url_path, auth=(server['username'], server['password']), headers={}, verify=False)
 
 if response.status_code != 200:
@@ -59,29 +46,24 @@ if response.status_code != 200:
         response.content
     sys.exit(1)
 else:
-    info = json.loads(response.content)
+    commits = json.loads(response.content)
 
     # build a map of the commit ids for each branch
     newCommit = {}
-    for branch in info["values"]:
-        branchid = branchName
-        lastcommit = branch["id"]
-        newCommit[branchid] = lastcommit
-        commitMsg = branch["message"]
+    commitId = commits["values"][0]["id"]
+    newCommit[branchName] = commitId
+    commitMsg = commits["values"][0]["message"]
 
     # trigger state is perisisted as json
     newTriggerState = json.dumps(newCommit)
 
     if triggerState != newTriggerState:
-        if len(triggerState) == 0:
-            oldCommit = {}
-        else:
-            oldCommit = json.loads(triggerState)
+        oldCommitId =''
+        if len(triggerState) != 0:
+            oldCommitId = json.loads(triggerState)[branchName]
 
-        branch, commitId = findNewCommit(oldCommit, newCommit)
-        fileChanges = getFileChanges(lastcommit)
+        fileChanges = getFileChanges(oldCommitId, commits)
 
-        if branchName == "" or (branchName != "" and branchName == branch):
-            triggerState = newTriggerState
+        triggerState = newTriggerState
 
-        print("Bitbucket triggered release for %s-%s" % (branch, commitId))
+        print("Bitbucket triggered release for %s-%s" % (branchName, commitId))
